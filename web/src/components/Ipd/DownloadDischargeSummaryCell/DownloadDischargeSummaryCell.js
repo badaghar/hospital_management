cbb288b0116aee1a2dbed24fa99793932c816c32
Abdul Kareem <rajab8321@gmail.com>
2023-09-01 19:15:26 +0530

    version 13.1.0 discharge summary corrected

--- web/src/components/Ipd/DownloadDischargeSummaryCell/DownloadDischargeSummaryCell.js
+++ web/src/components/Ipd/DownloadDischargeSummaryCell/DownloadDischargeSummaryCell.js
@@ -7,10 +7,18 @@ export const QUERY = gql`
         date_of_admission
         discharge_date
         patient{
-          name
-          age
-
+        name
+        age
+        address
+        phone_no
+        gender
         }
+        IpdConsultation{
+          consultation_doctor
+          consultation_type
+          amount
+        }
+
       }
     }
   }
@@ -25,6 +33,25 @@ export const Failure = ({ error }) => (
 )
 
 export const Success = ({ downloadDischargeSummary }) => {
+  console.log(downloadDischargeSummary.ipd.IpdConsultation)
+
+  const addNewLine = (value) => {
+    // if(typeof val!=='string'){
+    //   return []
+    // }
+    let val
+    try {
+      val = value.split('\n')
+
+    } catch (error) {
+      val = []
+
+    }
+    return val
+  }
+
+
+
   return (
     <>
       <section
@@ -32,13 +59,13 @@ export const Success = ({ downloadDischargeSummary }) => {
         style={{ width: '21cm', height: '29.7cm', padding: '1cm 1cm' }}
       >
         <section
-          className=""
+          className="relative"
           style={{ width: '18cm', height: '27.1cm' }}
         >
           <div style={{ padding: '' }}>
             <img src="/sd.jpg" alt="" srcset="" />
           </div>
-          <div className="m-3">
+          <div className="m-3 ">
             <div className="flex justify-center">
 
             </div>
@@ -61,7 +88,7 @@ export const Success = ({ downloadDischargeSummary }) => {
                     Sex :
                   </span>
                   <span className="mx-3 w-24">
-                    {downloadDischargeSummary.summary.gender}
+                    {downloadDischargeSummary.ipd.patient.gender}
                   </span>
                   {/* <input type="text" className="border border-black  ml-3 px-2" onChange={(e)=>change('gender',e.target.value)} value={obj.gender}/> */}
 
@@ -75,6 +102,41 @@ export const Success = ({ downloadDischargeSummary }) => {
                   </span>
                   {/* <input type="text" className="border border-black  ml-3 px-2 flex-1" onChange={(e)=>change('address',e.target.value)}  value={obj.address}/> */}
                 </div>
+
+
+
+                <div className="flex justify-between">
+                  <span>
+                    PID NO :
+                  </span>
+                  <span className="ml-3 ">
+                    SHM6745{downloadDischargeSummary.ipd.patient.name.split('(')[1].split(')')[0].trim()}
+                  </span>
+                  <span >
+                    IP. NO :
+                  </span>
+                  <span className="mx-3">
+                    SHM7960{downloadDischargeSummary.id}
+                  </span>
+                  <span >
+                    Ward :
+                  </span>
+                  <span className="mx-3 w-24">
+                    {/* {downloadDischargeSummary.ipd.Bed.bed_name} */}
+                  </span>
+                  {/* <input type="text" className="border border-black  ml-3 px-2" onChange={(e)=>change('gender',e.target.value)} value={obj.gender}/> */}
+
+                </div>
+
+
+
+
+
+
+
+
+
+
                 <div className="flex my-3 justify-between">
                   <div>
 
@@ -105,33 +167,157 @@ export const Success = ({ downloadDischargeSummary }) => {
                   {/* <input type="text" className="border border-black  ml-3 px-2 flex-1"  onChange={(e)=>change('refByDr',e.target.value)}  value={obj.refByDr} /> */}
                 </div>
 
-                <div className="flex flex-col h-24">
+                <div className="flex my-3">
+                  <span>
+                    Consultante (S)  :
+                  </span>
+
+                  <div className="flex flex-wrap space-x-2 ">
+
+                    {
+                      downloadDischargeSummary.ipd.IpdConsultation?.map((ele) => {
+                        return (
+
+                          <div>
+                            Dr. {ele.consultation_doctor}
+
+                          </div>
+                        )
+                      })
+                    }
+                  </div>
+                </div>
+
+                <div className="flex flex-col">
                   <span>Diagnosis</span>
                   <span>
-                    {downloadDischargeSummary.summary.diagnosis}
+                    {/* {downloadDischargeSummary.summary.diagnosis} */}
+                    {addNewLine(downloadDischargeSummary.summary.diagnosis)?.map((ele, ind, arr) => {
+                      return (
+                        <>
+                          {ele.trim()}
+                          {ind < arr.length - 1 && <br />}
+                        </>
+                      )
+                    })
+                    }
                   </span>
                   {/* <textarea name="" id=""  rows="2" className="border border-black  px-2 flex-1" onChange={(e)=>change('diagnosis',e.target.value)}   value={obj.diagnosis} ></textarea> */}
                 </div>
-                <div className="flex flex-col h-60">
+                <div className="flex flex-col">
                   <span>Chief Complaint & History : </span>
                   <span className="">
-                    {downloadDischargeSummary.summary.chief}
+                    {/* {downloadDischargeSummary.summary.chief} */}
+                    {addNewLine(downloadDischargeSummary.summary.chief)?.map((ele, ind, arr) => {
+                      return (
+                        <>
+                          {ele.trim()}
+                          {ind < arr.length - 1 && <br />}
+                        </>
+                      )
+                    })
+                    }
+
                   </span>
                   {/* <textarea name="" id=""  rows="7" className="border border-black  px-2 flex-1" onChange={(e)=>change('chief',e.target.value)}   value={obj.chief} ></textarea> */}
                 </div>
-                <div className="flex flex-col h-56">
+                <div className="flex flex-col">
                   <span>Clinical Findings : </span>
                   <span className="">
-                    {downloadDischargeSummary.summary.clinical}
+                    {/* {downloadDischargeSummary.summary.clinical} */}
+                    {addNewLine(downloadDischargeSummary.summary.clinical)?.map((ele, ind, arr) => {
+                      return (
+                        <>
+                          {ele.trim()}
+                          {ind < arr.length - 1 && <br />}
+                        </>
+                      )
+                    })
+                    }
                   </span>
                   {/* <textarea name="" id=""  rows="7" className="border border-black  px-2 flex-1" onChange={(e)=>change('clinical',e.target.value)}   value={obj.clinical} ></textarea> */}
                 </div>
                 <div className="flex flex-col">
                   <span>Investigations : </span>
                   <span className="">
-                    {downloadDischargeSummary.summary.investigations}
+                    {/* {downloadDischargeSummary.summary.investigations} */}
+                    {addNewLine(downloadDischargeSummary.summary.investigations)?.map((ele, ind, arr) => {
+                      return (
+                        <>
+                          {ele.trim()}
+                          {ind < arr.length - 1 && <br />}
+                        </>
+                      )
+                    })
+                    }
                   </span>
                   {/* <textarea name="" id=""  rows="7" className="border border-black  px-2 flex-1" onChange={(e)=>change('investigations',e.target.value)}  value={obj.investigations} ></textarea> */}
+                </div>
+
+                <div className="flex flex-col">
+                  <span>Treatement / Surgery / Delivery Notes : </span>
+                  <span>
+                    {addNewLine(downloadDischargeSummary.summary.surgery)?.map((ele, ind, arr) => {
+                      return (
+                        <>
+                          {ele.trim()}
+                          {ind < arr.length - 1 && <br />}
+                        </>
+                      )
+                    })
+                    }
+
+                  </span>
+                  {/* <textarea name="" id=""  rows="7" className="border border-black  px-2 flex-1" onChange={(e)=>change('surgery',e.target.value)}  value={obj.surgery}></textarea> */}
+                </div>
+
+                <div className="flex flex-col">
+                  <span>Treatement Advised at the time of Discharge : </span>
+                  <span>
+                    {addNewLine(downloadDischargeSummary.summary.treatement)?.map((ele, ind, arr) => {
+                      return (
+                        <>
+                          {ele.trim()}
+                          {ind < arr.length - 1 && <br />}
+                        </>
+                      )
+                    })
+                    }
+                  </span>
+
+                </div>
+
+
+
+                <div className="absolute bottom-0 w-full">
+                  <div className="grid grid-cols-2 ">
+                    <div>
+                      <span>
+                        consultant
+                      </span>
+                      <div>
+                        Date :
+                      </div>
+                    </div>
+
+
+
+                    <div className="justify-self-end">
+
+                      <span>
+                        Registrar/Resident M.O
+
+                      </span>
+                      <div>
+                        Ph No:
+                      </div>
+                    </div>
+
+
+
+
+                  </div>
+
                 </div>
 
 
@@ -140,37 +326,15 @@ export const Success = ({ downloadDischargeSummary }) => {
 
             </div>
 
+
+
+
+
+
           </div>
         </section>
       </section >
 
-      <section
-        className="border-black border"
-        style={{ width: '21cm', height: '29.7cm', padding: '1cm 1cm' }}
-      >
-        <section
-          className=""
-          style={{ width: '18cm', height: '27.1cm' }}
-        >
-          <div className="flex flex-col h-96">
-            <span>Treatement / Surgery / Delivery Notes : </span>
-            <span>
-            {downloadDischargeSummary.summary.surgery}
-
-            </span>
-            {/* <textarea name="" id=""  rows="7" className="border border-black  px-2 flex-1" onChange={(e)=>change('surgery',e.target.value)}  value={obj.surgery}></textarea> */}
-          </div>
-          <div className="flex flex-col">
-            <span>Treatement Advised at the time of Discharge : </span>
-            <span>
-            {downloadDischargeSummary.summary.treatement}
-            </span>
-
-          </div>
-
-
-        </section>
-      </section >
 
     </>
   )
--- web/src/components/Ipd/SummaryIpd/SummaryIpd.js
+++ web/src/components/Ipd/SummaryIpd/SummaryIpd.js
@@ -2,7 +2,7 @@ import { navigate, routes } from "@redwoodjs/router"
 import { useMutation } from "@redwoodjs/web"
 import { toast } from "@redwoodjs/web/dist/toast"
 import axios from "axios"
-import { useState,useLayoutEffect } from "react"
+import { useState, useLayoutEffect } from "react"
 // import { useLayoutEffect } from "react-js-dialog-box"
 
 const CREATE_IPD_SUMMARY_MUTATION = gql`
@@ -16,8 +16,8 @@ const CREATE_IPD_SUMMARY_MUTATION = gql`
 const SummaryIpd = ({ ipd }) => {
 
   const [obj, setObj] = useState({})
-  const [isPrint,setIsPrint] = useState(false)
-  const [isUpdate,setIsUpdate] = useState(!!ipd.IpdSummary[0])
+  const [isPrint, setIsPrint] = useState(false)
+  const [isUpdate, setIsUpdate] = useState(!!ipd.IpdSummary[0])
   function getPDF(id) {
     return axios.get(
       `/.redwood/functions/downloadDischargeSummary?id=` +
@@ -35,17 +35,17 @@ const SummaryIpd = ({ ipd }) => {
       .then((response) => {
         const blob = new Blob([response.data], { type: 'application/pdf' })
         var blobURL = URL.createObjectURL(blob)
-        var iframe =  document.createElement('iframe')
+        var iframe = document.createElement('iframe')
         document.body.appendChild(iframe)
         iframe.style.display = 'none'
 
         iframe.src = blobURL
-     iframe.onload = function() {
-      setTimeout(function() {
-        iframe.focus()
-        iframe.contentWindow.print()
-      }, 1)
-    }
+        iframe.onload = function () {
+          setTimeout(function () {
+            iframe.focus()
+            iframe.contentWindow.print()
+          }, 1)
+        }
         toast.success('Download Complete')
       })
       .catch((err) => {
@@ -54,27 +54,25 @@ const SummaryIpd = ({ ipd }) => {
       })
   }
 
-  useLayoutEffect(()=>{
-    if(ipd.IpdSummary[0])
-    {
-      setObj(()=>{
-         return { ...ipd.IpdSummary[0].summary}
+  useLayoutEffect(() => {
+    if (ipd.IpdSummary[0]) {
+      setObj(() => {
+        return { ...ipd.IpdSummary[0].summary }
       })
 
     }
-  },[ipd])
+  }, [ipd])
   const [createIpdSummary, { loading, error }] = useMutation(
     CREATE_IPD_SUMMARY_MUTATION,
     {
       onCompleted: (data) => {
         toast.success('IpdSummary created')
-        if(isPrint)
-        {
+        if (isPrint) {
           printPDF(data.createIpdSummary.id)
 
 
         }
-        navigate(routes.ipds({type:'IPD'}))
+        navigate(routes.ipds({ type: 'IPD' }))
       },
       onError: (error) => {
         toast.error(error.message)
@@ -91,39 +89,37 @@ const SummaryIpd = ({ ipd }) => {
     });
   }
   const save = () => {
-      let input = {
-        ipdId : ipd.id,
-        summary: obj
-      }
-      if(isUpdate)
-      {
-        // console.log(ipd.IpdSummary[0])
-        input['update'] = 1
-        input['id'] = ipd.IpdSummary[0].id
-      }
-      else{
-        input['update'] = 0
-        input['id'] = 0
-      }
-      createIpdSummary({ variables: { input } })
+    let input = {
+      ipdId: ipd.id,
+      summary: obj
+    }
+    if (isUpdate) {
+      // console.log(ipd.IpdSummary[0])
+      input['update'] = 1
+      input['id'] = ipd.IpdSummary[0].id
+    }
+    else {
+      input['update'] = 0
+      input['id'] = 0
+    }
+    createIpdSummary({ variables: { input } })
   }
   const saveAndPrint = () => {
-      const input = {
-        ipdId : ipd.id,
-        summary: obj
-      }
-      if(isUpdate)
-      {
-        // console.log(ipd.IpdSummary[0])
-        input['update'] = 1
-        input['id'] = ipd.IpdSummary[0].id
-      }
-      else{
-        input['update'] = 0
-        input['id'] = 0
-      }
-      setIsPrint(true)
-      createIpdSummary({ variables: { input } })
+    const input = {
+      ipdId: ipd.id,
+      summary: obj
+    }
+    if (isUpdate) {
+      // console.log(ipd.IpdSummary[0])
+      input['update'] = 1
+      input['id'] = ipd.IpdSummary[0].id
+    }
+    else {
+      input['update'] = 0
+      input['id'] = 0
+    }
+    setIsPrint(true)
+    createIpdSummary({ variables: { input } })
 
   }
   return (
@@ -149,14 +145,17 @@ const SummaryIpd = ({ ipd }) => {
             <span>
               Sex :
             </span>
-            <input type="text" className="border border-black  ml-3 px-2" onChange={(e)=>change('gender',e.target.value)} value={obj.gender}/>
+            <span>
+              {ipd.patient.gender}
+            </span>
+            {/* <input type="text" className="border border-black  ml-3 px-2" onChange={(e)=>change('gender',e.target.value)} value={obj.gender}/> */}
 
           </div>
           <div className="flex my-3">
             <span>
               Address :
             </span>
-            <input type="text" className="border border-black  ml-3 px-2 flex-1" onChange={(e)=>change('address',e.target.value)}  value={obj.address}/>
+            <input type="text" className="border border-black  ml-3 px-2 flex-1" onChange={(e) => change('address', e.target.value)} value={obj.address} />
           </div>
           <div className="flex my-3">
             <span>
@@ -176,32 +175,54 @@ const SummaryIpd = ({ ipd }) => {
             <span>
               Ref by Dr  :
             </span>
-            <input type="text" className="border border-black  ml-3 px-2 flex-1"  onChange={(e)=>change('refByDr',e.target.value)}  value={obj.refByDr} />
+            <input type="text" className="border border-black  ml-3 px-2 flex-1" onChange={(e) => change('refByDr', e.target.value)} value={obj.refByDr} />
           </div>
+          <div className="flex my-3">
+            <span>
+              Consultante (S)  :
+            </span>
+
+            <div className="flex flex-wrap space-x-2 ">
+
+              {
+                ipd.IpdConsultation?.map((ele) => {
+                  return (
+
+                    <div>
+                     Dr. {ele.consultation_doctor}
+
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </div>
+
+
 
           <div className="flex flex-col">
             <span>Diagnosis</span>
-            <textarea name="" id=""  rows="2" className="border border-black  px-2 flex-1" onChange={(e)=>change('diagnosis',e.target.value)}   value={obj.diagnosis} ></textarea>
+            <textarea name="" id="" rows="2" className="border border-black  px-2 flex-1" onChange={(e) => change('diagnosis', e.target.value)} value={obj.diagnosis} ></textarea>
           </div>
           <div className="flex flex-col">
             <span>Chief Complaint & History : </span>
-            <textarea name="" id=""  rows="7" className="border border-black  px-2 flex-1" onChange={(e)=>change('chief',e.target.value)}   value={obj.chief} ></textarea>
+            <textarea name="" id="" rows="2" className="border border-black  px-2 flex-1" onChange={(e) => change('chief', e.target.value)} value={obj.chief} ></textarea>
           </div>
           <div className="flex flex-col">
             <span>Clinical Findings : </span>
-            <textarea name="" id=""  rows="7" className="border border-black  px-2 flex-1" onChange={(e)=>change('clinical',e.target.value)}   value={obj.clinical} ></textarea>
+            <textarea name="" id="" rows="2" className="border border-black  px-2 flex-1" onChange={(e) => change('clinical', e.target.value)} value={obj.clinical} ></textarea>
           </div>
           <div className="flex flex-col">
             <span>Investigations : </span>
-            <textarea name="" id=""  rows="7" className="border border-black  px-2 flex-1" onChange={(e)=>change('investigations',e.target.value)}  value={obj.investigations} ></textarea>
+            <textarea name="" id="" rows="2" className="border border-black  px-2 flex-1" onChange={(e) => change('investigations', e.target.value)} value={obj.investigations} ></textarea>
           </div>
           <div className="flex flex-col">
             <span>Treatement / Surgery / Delivery Notes : </span>
-            <textarea name="" id=""  rows="7" className="border border-black  px-2 flex-1" onChange={(e)=>change('surgery',e.target.value)}  value={obj.surgery}></textarea>
+            <textarea name="" id="" rows="3" className="border border-black  px-2 flex-1" onChange={(e) => change('surgery', e.target.value)} value={obj.surgery}></textarea>
           </div>
           <div className="flex flex-col">
             <span>Treatement Advised at the time of Discharge : </span>
-            <textarea name="" id=""  rows="7" className="border border-black  px-2 flex-1" onChange={(e)=>change('treatement',e.target.value)}   value={obj.treatement}></textarea>
+            <textarea name="" id="" rows="6" className="border border-black  px-2 flex-1" onChange={(e) => change('treatement', e.target.value)} value={obj.treatement}></textarea>
           </div>
 
           <div className="flex justify-center m-2">
