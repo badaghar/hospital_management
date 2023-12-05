7513694444e056d1b7828b06dfe89edb12b06a8d
Abdul Kareem <rajab8321@gmail.com>
2023-12-05 16:25:29 +0530

    very minor change 2

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -5,10 +5,9 @@ module.exports = {
     extend: {
       gridTemplateColumns: {
         '18': 'repeat(18, minmax(0, 1fr))',
-      },
-      gridTemplateColumns: {
         '13': 'repeat(13, minmax(0, 1fr))',
       },
+
       backgroundImage: {
         'logo': "url('../public/BHJ_log.jpg')",
       }
