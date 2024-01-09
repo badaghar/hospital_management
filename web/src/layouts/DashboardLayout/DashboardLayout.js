2eef679aeb8c04ca5a784d9863281bd821c9f996
Abdul Kareem <rajab8321@gmail.com>
2024-01-09 23:10:28 +0530

    version 10.0.1 issue 2 in progress

--- web/src/layouts/DashboardLayout/DashboardLayout.js
+++ web/src/layouts/DashboardLayout/DashboardLayout.js
@@ -73,6 +73,27 @@ const DashboardLayout = ({ children }) => {
 
 
                          {/* {(currentUser.permissions?.charges?.length > 0 || isAdmin) && */}
+
+                         {
+                          <>
+
+                          <li className="px-5 hidden md:block">
+                      <div className="flex flex-row items-center mt-5 h-8">
+                        <div className="text-sm font-light tracking-wide text-gray-400 uppercase">Reception</div>
+                      </div>
+                    </li>
+
+                    <li>
+                      <Link to={routes.newIpd({type:'OPD'})} className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-gray-800 pr-6">
+                        <span className="inline-flex justify-center items-center ml-4">
+                          <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
+                        </span>
+                        <span className="ml-2 text-sm tracking-wide truncate">Add Patient</span>
+                      </Link>
+                    </li>
+
+                          </>
+                         }
                          <>
                   <li className="px-5 hidden md:block">
                     <div className="flex flex-row items-center h-8">
