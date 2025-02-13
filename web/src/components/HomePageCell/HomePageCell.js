975ecf78c1d809da3b3e9fabdfb678c61495830e
abdulkareem97 <rajab8321@gmail.com>
2025-02-14 05:09:08 +0530

    version 1.0.0

--- web/src/components/HomePageCell/HomePageCell.js
+++ web/src/components/HomePageCell/HomePageCell.js
@@ -1,25 +1,23 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 
 export const QUERY = gql`
   query FindHomePageQuery {
-    purchaseMedicines{
+    purchaseMedicines {
       id
       grand_total
       date
-    },
-    saleMedicines{
+    }
+    saleMedicines {
       id
       grand_total
       date
-
-    },
-    ipds(type: "IPD"){
+    }
+    ipds(type: "IPD") {
       id
     }
-    opds:ipds(type: "OPD"){
+    opds: ipds(type: "OPD") {
       id
     }
-
   }
 `
 
@@ -47,7 +45,10 @@ export const Success = ({ purchaseMedicines, saleMedicines, ipds, opds }) => {
   // const [totalPurchase,setTotalPurchase] = useState(0)
 
   useEffect(() => {
-    const tp = purchaseMedicines.reduce((prev, item) => prev + item.grand_total, 0)
+    const tp = purchaseMedicines.reduce(
+      (prev, item) => prev + item.grand_total,
+      0
+    )
     setTotalPurchase(tp)
     const ts = saleMedicines.reduce((prev, item) => prev + item.grand_total, 0)
     setTotalSale(ts)
@@ -55,6 +56,10 @@ export const Success = ({ purchaseMedicines, saleMedicines, ipds, opds }) => {
     const today = new Date()
     const month = new Date()
     month.setDate(1)
+    month.setHours(0)
+    month.setMinutes(0)
+    month.setSeconds(0)
+
     today.setHours(0)
     today.setMinutes(0)
     today.setSeconds(0)
@@ -81,59 +86,52 @@ export const Success = ({ purchaseMedicines, saleMedicines, ipds, opds }) => {
       // console.log(newDate,today)
       return newDate >= month
     })
+    console.log(top, monthtop)
 
     const msale = monthtos.reduce((prev, item) => prev + item.grand_total, 0)
-    const mpurchase = monthtop.reduce((prev, item) => prev + item.grand_total, 0)
+    const mpurchase = monthtop.reduce(
+      (prev, item) => prev + item.grand_total,
+      0
+    )
 
     setTodaySale(tSale)
     setTodayPurchase(tPurchase)
     setMonthPurchase(mpurchase)
     setMonthSale(msale)
     // console.log(tos)
-
   }, [])
   return (
     <>
-      <div className="flex m-4 flex-wrap relative text-white">
-        <div className="flex flex-col bg-orange-800 shadow-lg rounded-2xl p-6 items-center m-4">
+      <div className="relative m-4 flex flex-wrap text-white">
+        <div className="m-4 flex flex-col items-center rounded-2xl bg-orange-800 p-6 shadow-lg">
           <h1>Total Profit</h1>
           <p> ₹ {profit.toFixed(2)} </p>
-
         </div>
-        <div className="flex flex-col bg-green-800 shadow-lg rounded-2xl p-6 items-center m-4">
+        <div className="m-4 flex flex-col items-center rounded-2xl bg-green-800 p-6 shadow-lg">
           <h1>Total Purchase Medicine</h1>
           <p> ₹ {totalPurchase.toFixed(2)} </p>
-
         </div>
-        <div className="flex flex-col bg-red-500 shadow-lg rounded-2xl p-6 items-center m-4">
+        <div className="m-4 flex flex-col items-center rounded-2xl bg-red-500 p-6 shadow-lg">
           <h1>Total Sale Medicine</h1>
           <p> ₹ {totalSale.toFixed(2)} </p>
-
         </div>
-        <div className="flex flex-col bg-purple-700 shadow-lg rounded-2xl p-6 items-center m-4">
+        <div className="m-4 flex flex-col items-center rounded-2xl bg-purple-700 p-6 shadow-lg">
           <h1>Today Purchase Medicine</h1>
           <p> ₹ {todayPurchase.toFixed(2)} </p>
-
         </div>
-        <div className="flex flex-col bg-gray-800  shadow-lg rounded-2xl p-6 items-center m-4">
+        <div className="m-4 flex flex-col items-center rounded-2xl bg-gray-800 p-6 shadow-lg">
           <h1>Today Sale Medicine</h1>
           <p> ₹ {todaySale.toFixed(2)} </p>
-
         </div>
-        <div className="flex flex-col bg-purple-950 shadow-lg rounded-2xl p-6 items-center m-4">
+        <div className="m-4 flex flex-col items-center rounded-2xl bg-purple-950 p-6 shadow-lg">
           <h1>This Month Purchase Medicine</h1>
           <p> ₹ {monthPurchase.toFixed(2)} </p>
-
         </div>
-        <div className="flex flex-col bg-[#AED2FF] shadow-lg rounded-2xl p-6 items-center m-4">
+        <div className="m-4 flex flex-col items-center rounded-2xl bg-[#AED2FF] p-6 shadow-lg">
           <h1>This Month Sale Medicine</h1>
           <p> ₹ {monthSale.toFixed(2)} </p>
-
         </div>
-
       </div>
-
-
     </>
   )
 }
--- web/src/components/SaleMedicine/DownloadSaleMedicine/DownloadSaleMedicine.js
+++ web/src/components/SaleMedicine/DownloadSaleMedicine/DownloadSaleMedicine.js
@@ -6,18 +6,16 @@ import BarLoader from 'react-spinners/BarLoader'
 import { Link, routes, navigate } from '@redwoodjs/router'
 import { useMutation } from '@redwoodjs/web'
 import { toast } from '@redwoodjs/web/toast'
+
 import SaleMedicineSkeleton from '../SaleMedicineSkeleton/SaleMedicineSkeleton'
 
 const axios = require('axios')
 
-
-const DownloadSaleMedicine = ({ saleMedicine,download }) => {
-
+const DownloadSaleMedicine = ({ saleMedicine, download }) => {
   const [downloadstart, setDownloadstart] = useState(false)
   function getPDF() {
     return axios.get(
-      `/.redwood/functions/downloadSaleMedicineBill?id=` +
-      saleMedicine.id,
+      `/.redwood/functions/downloadSaleMedicineBill?id=` + saleMedicine.id,
       {
         responseType: 'arraybuffer',
         headers: {
@@ -27,82 +25,93 @@ const DownloadSaleMedicine = ({ saleMedicine,download }) => {
     )
   }
   const printPDF = () => {
-    setDownloadstart(true)
-    return getPDF() // API call
-      .then((response) => {
-        const blob = new Blob([response.data], { type: 'application/pdf' })
-        // const link = document.createElement('a')
-        // link.href = window.URL.createObjectURL(blob)
-        // link.download =
-        //   saleMedicine.patient.name + saleMedicine.id +
-        //   '.pdf'
-        // link.click()
-        var blobURL = URL.createObjectURL(blob)
-        var iframe =  document.createElement('iframe')
-        document.body.appendChild(iframe)
-        iframe.style.display = 'none'
+    const printContents = document.getElementById('printable').innerHTML
+    const originalContents = document.body.innerHTML
 
-        iframe.src = blobURL
-     iframe.onload = function() {
-      setTimeout(function() {
-        iframe.focus()
-        iframe.contentWindow.print()
-      }, 1)
-    }
-       setDownloadstart(false)
-        toast.success('Download Complete')
-      })
-      .catch((err) => {
-      setDownloadstart(false)
-        toast.error('something wrong happened try again')
-        console.log(err)
-      })
+    document.body.innerHTML = printContents
+    window.print()
+    // document.body.innerHTML = originalContents
+    window.location.reload()
+
+    // wwindo
+    // setDownloadstart(true)
+    // return getPDF() // API call
+    //   .then((response) => {
+    //     const blob = new Blob([response.data], { type: 'application/pdf' })
+    //     // const link = document.createElement('a')
+    //     // link.href = window.URL.createObjectURL(blob)
+    //     // link.download =
+    //     //   saleMedicine.patient.name + saleMedicine.id +
+    //     //   '.pdf'
+    //     // link.click()
+    //     var blobURL = URL.createObjectURL(blob)
+    //     var iframe =  document.createElement('iframe')
+    //     document.body.appendChild(iframe)
+    //     iframe.style.display = 'none'
+
+    //     iframe.src = blobURL
+    //  iframe.onload = function() {
+    //   setTimeout(function() {
+    //     iframe.focus()
+    //     iframe.contentWindow.print()
+    //   }, 1)
+    // }
+    //    setDownloadstart(false)
+    //     toast.success('Download Complete')
+    //   })
+    //   .catch((err) => {
+    //   setDownloadstart(false)
+    //     toast.error('something wrong happened try again')
+    //     console.log(err)
+    //   })
   }
   return (
     <>
-    {!download ? (
-      <>
-        <div className="grid grid-cols-12 gap-y-4 w-full justify-center m-2 p-2">
-          <div className="col-span-3"></div>
-          <div className="flex justify-start col-span-3 p-2">
-            <Link
-              to={routes.saleMedicines()}
-              className="bg-red-900 text-white hover:bg-red-700 font-bold py-2 px-4 rounded-full"
-            >
-              Go Back
-            </Link>
-          </div>
-          <div className="flex col-span-3 justify-end p-2">
-            <button
-              className="bg-red-900 text-white hover:bg-red-700 font-bold py-2 px-4 rounded-full"
-              id="print-button"
-              onClick={printPDF}
-              disabled={downloadstart}
-            >
-              {downloadstart ? (
-                <>
-                  <BarLoader
-                    color="white"
-                    loading={downloadstart}
-                    height={10}
-                  />
-                </>
-              ) : (
-                'Print PDF'
-              )}
-            </button>
-          </div>
+      {!download ? (
+        <>
+          <div className="m-2 grid w-full grid-cols-12 justify-center gap-y-4 p-2">
+            <div className="col-span-3"></div>
+            <div className="col-span-3 flex justify-start p-2">
+              <Link
+                to={routes.saleMedicines()}
+                className="rounded-full bg-red-900 px-4 py-2 font-bold text-white hover:bg-red-700"
+              >
+                Go Back
+              </Link>
+            </div>
+            <div className="col-span-3 flex justify-end p-2">
+              <button
+                className="rounded-full bg-red-900 px-4 py-2 font-bold text-white hover:bg-red-700"
+                id="print-button"
+                onClick={printPDF}
+                disabled={downloadstart}
+              >
+                {downloadstart ? (
+                  <>
+                    <BarLoader
+                      color="white"
+                      loading={downloadstart}
+                      height={10}
+                    />
+                  </>
+                ) : (
+                  'Print PDF'
+                )}
+              </button>
+            </div>
 
-          <div className="col-span-12 flex justify-center">
-            {<SaleMedicineSkeleton saleMedicine={saleMedicine}/>}
+            <div className="col-span-12 flex justify-center">
+              <div id="printable">
+                {<SaleMedicineSkeleton saleMedicine={saleMedicine} />}
+              </div>
+            </div>
           </div>
-        </div>
-      </>
-    ) : (
-      <>{<SaleMedicineSkeleton saleMedicine={saleMedicine}/>}</>
-    )}
-  </>
-)
+        </>
+      ) : (
+        <>{<SaleMedicineSkeleton saleMedicine={saleMedicine} />}</>
+      )}
+    </>
+  )
 }
 
 export default DownloadSaleMedicine
